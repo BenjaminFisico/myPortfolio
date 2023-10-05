3ad32b823caa7a83c2ee52cf59b4bad39365b529
benjamin fisico <benjaminfisico@gmail.com>
2023-10-05 12:35:51 -0300

    Routes changes

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -13,9 +13,9 @@ export class HomepageComponent {
   <p>Después de terminar el secundario, en el 2017 pude consolidar los conocimientos que había adquirido durante esos años, gracias al curso 111mil programadores, donde aprendí la base del pensamiento computacional y los fundamentos de la POO.</p> \
   <p>Mi formación académica concluye al graduarme de una tecnicatura en análisis de sistemas, en el 2022, donde puedo decir que me construí como profesional, no solo a nivel de conocimientos técnicos, si no también, en cuanto a habilidades de comunicación y trabajo en equipo.</p> \
   <hr>\
-  <p>Gracias por visitar mi portfolio aquí podrás encontrar los <strong><a href="/home/proyects">proyectos</a></strong> de relevancia que he realizado o que iré realizando a lo largo del tiempo.”</p> \
-  <p>También cree una sección para las <strong><a href="/home/works">experiencias laborales</a></strong> adquiridas, donde incluí mi tech stack.</p> \
-  <p> Si mi perfil llama tu atención, no dudes en <strong><a href="/home/contact">contactarme</a></strong>.</p> \
+  <p>Gracias por visitar mi portfolio aquí podrás encontrar los <strong><a href="/myPortfolio/home/proyects">proyectos</a></strong> de relevancia que he realizado o que iré realizando a lo largo del tiempo.”</p> \
+  <p>También cree una sección para las <strong><a href="/myPortfolio/home/works">experiencias laborales</a></strong> adquiridas, donde incluí mi tech stack.</p> \
+  <p> Si mi perfil llama tu atención, no dudes en <strong><a href="/myPortfolio/home/contact">contactarme</a></strong>.</p> \
   <p> <strong>¡Juntos podemos lograr cosas increíbles!</strong></p>';
 
 }
